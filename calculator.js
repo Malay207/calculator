ff7d52cc63e56988a667d4ecfd3df71a81394132
malay <malaydev207@gmail.com>
2023-04-09 23:27:54 +0530

    initial commite

---
+++ calculator.js
@@ -0,0 +1,22 @@
+window.onload =
+    function () {
+
+        alert("hello let's calculate")
+    }
+function clean() {
+    document.form.text.value = "";
+}
+function insert(num) {
+    document.form.text.value = document.form.text.value + num;
+}
+function equal() {
+    let exp = document.form.text.value;
+    if (exp) {
+        document.form.text.value = eval(exp);
+    }
+}
+function back() {
+    let expt = document.form.text.value;
+    document.form.text.value = expt.substring(0, expt.length - 1);
+
+}
